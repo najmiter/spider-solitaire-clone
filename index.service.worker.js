6fe147046a8d5dea7b46394be5d465e38292c439
Najam <realnajmiter@gmail.com>
2026-05-02 23:45:30 +0500

    and now we got hints too

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777742934|7585331870';
+const CACHE_VERSION = '1777747479|12130019894';
 /** @type {string} */
 const CACHE_PREFIX = 'Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
