a3a361ffba51a94b01d7f6a03f1c773fb90492a3
Najam <realnajmiter@gmail.com>
2026-05-03 20:43:28 +0500

    z index glitch fixes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777813312|416743594';
+const CACHE_VERSION = '1777822954|1979995787';
 /** @type {string} */
 const CACHE_PREFIX = 'Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
